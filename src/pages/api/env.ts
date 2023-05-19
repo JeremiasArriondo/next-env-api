2e03e24a185efcbd3813a761aeccd6cb0680a5dc
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-05-18 23:47:25 -0300

    feat: first commit

---
+++ src/pages/api/env.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  env: string,
+  table: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const environment = process.env.NODE_ENV
+  const table = process.env.TABLE_NAME
+
+  res.status(200).json({ env: environment, table: table})
+}
